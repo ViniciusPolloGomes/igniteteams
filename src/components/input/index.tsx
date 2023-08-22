10ab09600a6911d5a4252c2767eda41df233d95e
Vinicius Pollo Gomes <ViniciusPolloGomes@gmail.com>
2023-08-21 21:33:16 -0300

    Css helper  *All  styles.ts

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,9 +1,14 @@
 import {TextInputProps} from 'react-native'
 import {Container} from './styles'
+import { useTheme } from 'styled-components/native'
 
 export function Input({...rest}:TextInputProps ){
+    const {COLORS} = useTheme();
+
     return(
         <Container
+            placeholder="Nome da Turma."
+            placeholderTextColor={COLORS.GRAY_300}
             {...rest}
         />
     )
