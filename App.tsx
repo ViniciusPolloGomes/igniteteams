24b51121ec5bc7f7a350932c20b5b2d584bcab1c
Vinicius Pollo Gomes <ViniciusPolloGomes@gmail.com>
2023-08-22 19:52:55 -0300

    Create Page "Players", index.tsx and styles.ts

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import theme from './src/theme/theme'
 import {Loading} from '@components/Loading'
 import { StatusBar } from 'react-native';
 import { NewGroup } from '@components/NewGroup';
+import { Players } from '@screens/Players';
 
 export default function App() {
   const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold});
@@ -15,7 +16,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <NewGroup/> : <Loading/>}
+      {fontsLoaded ? <Players/> : <Loading/>}
     </ThemeProvider>
   );
 }
