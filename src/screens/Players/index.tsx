7dca48e078cce39e95750a8e47d1aaf60fcbe1f5
Vinicius Pollo Gomes <ViniciusPolloGomes@gmail.com>
2023-08-22 21:09:43 -0300

    Create ButtonIcon and typescript

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,6 +1,8 @@
 import { Header } from "@components/Header";
 import { Container } from "./styles";
 import { HighLight } from "@components/HighLight";
+import { ButtonIcon } from "@components/ButtonIcon";
+import { Input } from "@components/input";
 
 export function Players(){
     return(
@@ -10,6 +12,11 @@ export function Players(){
                 title="Nome da turma"
                 subtitle="adicione a galera e separe as turmas."
             />
+            <Input
+                placeholder="Nome da pessoa"
+                autoCorrect={false}
+            />
+            <ButtonIcon icon="add"/>
         </Container>
     )
 }
