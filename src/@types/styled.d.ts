1b5bae01acd6bcd47308754915d2cf5133f33e9d
Vinicius Pollo Gomes <ViniciusPolloGomes@gmail.com>
2024-02-23 11:43:59 -0300

    UPDATE PACKAGES 2024 EXPO

--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -1,11 +1,8 @@
-import 'styled-components';
+import 'styled-components/native';
 import theme from '../theme/theme';
 
+declare module 'styled-components/native' {
+  type ThemeType = typeof theme;
 
-declare module 'styled-components' {
-    type ThemeType = typeof theme;
-
-    export interface DefaultTheme extends ThemeType{}
+  export interface DefaultTheme extends ThemeType { }
 }
-
-
