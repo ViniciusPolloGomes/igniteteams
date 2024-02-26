5d6d6b9b524fe045fc51656aca3edb676f7ea033
Vinicius Pollo Gomes <ViniciusPolloGomes@gmail.com>
2024-02-26 11:14:26 -0300

    Correction Error

--- src/storage/group/groupCreate.ts
+++ src/storage/group/groupCreate.ts
@@ -1,5 +1,5 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
-import { GROUP_COLLECTION } from '@storage/storageconfig';
+import { GROUP_COLLECTION } from '@storage/storageConfig';
 import { groupsGetAll } from './groupGetAll';
 
 export async function groupCreate(newGroup: string){
@@ -7,7 +7,7 @@ export async function groupCreate(newGroup: string){
         const storedGroups = await groupsGetAll();
 
         const storage = JSON.stringify([...storedGroups, newGroup])
-        
+
         await AsyncStorage.setItem(GROUP_COLLECTION, storage);
 
     }catch(error){
