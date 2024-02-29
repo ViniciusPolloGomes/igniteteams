f00866928c02f94cfe6d55a0d226a1b8be6ec77c
rodrigo <rodrigo.rgtic@gmail.com>
2024-02-29 11:13:43 -0300

    useCallback

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,66 +1,53 @@
-import { Container} from './styles';
-import { Header } from '@components/Header';
-import {HighLight} from '@components/HighLight';
-import {GroupCard} from '@components/GroupCard'
-import {  useState , useCallback } from 'react';
-import {FlatList} from 'react-native';
-import { ListEmpty } from '@components/ListEmpty';
-import { Button } from '@components/Button';
-import { useNavigation, useFocusEffect} from '@react-navigation/native';
-import { groupsGetAll } from '@storage/group/groupGetAll';
+import { Container } from "./styles"
+import { Header } from "@components/Header"
+import { HighLight } from "@components/HighLight"
+import { GroupCard } from "@components/GroupCard"
+import { useState, useCallback } from "react"
+import { FlatList } from "react-native"
+import { ListEmpty } from "@components/ListEmpty"
+import { Button } from "@components/Button"
+import { useNavigation, useFocusEffect } from "@react-navigation/native"
+import { groupsGetAll } from "@storage/group/groupGetAll"
 
+export function Groups() {
+  const [groups, setGroups] = useState<string[]>([])
 
-export  function Groups() {
-  const [groups,setGroups] = useState<string[]>([]);
-  
-  const navigation = useNavigation();
-  function handleNewGroup (){
-    navigation.navigate('new')
+  const navigation = useNavigation()
+  function handleNewGroup() {
+    navigation.navigate("new")
   }
 
-  async function fetchGroup(){
-    try{
-      const data = await groupsGetAll();
+  async function fetchGroup() {
+    try {
+      const data = await groupsGetAll()
       setGroups(data)
-    }catch(error){
+    } catch (error) {
       console.log(error)
     }
   }
 
-  useFocusEffect(useCallback(
-      ()=>{
-        fetchGroup();
-      },[])
-  );
+  useFocusEffect(
+    useCallback(() => {
+      console.log("Atualizando lista de grupos")
+      fetchGroup()
+    }, [])
+  )
 
   return (
     <Container>
-        <Header/>
-        <HighLight
-          title="Turmas"
-          subtitle="jogue com a sua turma"
-        />
+      <Header />
+      <HighLight title="Turmas" subtitle="jogue com a sua turma" />
 
-        <FlatList
-          data={groups}
-          keyExtractor={item => item}
-          renderItem={({item})=> (
-            <GroupCard 
-              title={item}
-            />
-          )}
-          contentContainerStyle={groups.length === 0 && {flex : 1 }}
-          ListEmptyComponent={()=>(
-            <ListEmpty
-              message="Que tal cadastrar uma nova turma?"
-            />
-          )}
-        />
-      <Button
-        title="Criar nova Turma"
-        onPress={handleNewGroup}
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar uma nova turma?" />
+        )}
       />
+      <Button title="Criar nova Turma" onPress={handleNewGroup} />
     </Container>
-  );
+  )
 }
-
--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,47 +1,37 @@
-import { Header } from "@components/Header";
-import { Container , Content, Icon} from "./styles";
-import { HighLight } from "@components/HighLight";
-import { Button } from "@components/Button";
-import { Input } from "@components/input";
-import { useState} from 'react'
-import { useNavigation } from "@react-navigation/native";
-import {groupCreate} from '../../storage/group/groupCreate';
+import { Header } from "@components/Header"
+import { Container, Content, Icon } from "./styles"
+import { HighLight } from "@components/HighLight"
+import { Button } from "@components/Button"
+import { Input } from "@components/input"
+import { useState } from "react"
+import { useNavigation } from "@react-navigation/native"
+import { groupCreate } from "../../storage/group/groupCreate"
 
+export function NewGroup() {
+  const [group, setGroup] = useState("")
+  const navigation = useNavigation()
 
-
-export function NewGroup (){
-
-    const [group, setGroup] = useState('')
-    const navigation = useNavigation();
-
-    async function handleNew (){
-        try{ 
-            await groupCreate(group);
-            navigation.navigate('players', group )
-        }catch(error){
-            console.log(error)
-        }    
+  async function handleNew() {
+    try {
+      await groupCreate(group)
+      navigation.navigate("players", { group })
+    } catch (error) {
+      console.log(error)
     }
+  }
 
-    return(
-        <Container>
-            <Header showBackButton/>
-            <Content>
-                <Icon/>
-                <HighLight
-                    title="Nova Turma"
-                    subtitle="Crie a turma para adicionar as pessoas"
-                />
-                <Input
-                    placeholder="Nome da turma"
-                    onChangeText={setGroup}
-                />
-                <Button 
-                    title="Criar"
-                    onPress={handleNew}
-                />
-            </Content>
-        </Container>
-    )
+  return (
+    <Container>
+      <Header showBackButton />
+      <Content>
+        <Icon />
+        <HighLight
+          title="Nova Turma"
+          subtitle="Crie a turma para adicionar as pessoas"
+        />
+        <Input placeholder="Nome da turma" onChangeText={setGroup} />
+        <Button title="Criar" onPress={handleNew} />
+      </Content>
+    </Container>
+  )
 }
-
